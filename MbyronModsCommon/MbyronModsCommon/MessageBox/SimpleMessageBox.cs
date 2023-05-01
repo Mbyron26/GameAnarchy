0d8cc4c9e384ac46137cb05493b8badd879d7e6a
Mbyron26 <Mbyron26@outlook.com>
2023-05-01 14:56:38 +0800

    Updated XMLWariningMessageBox.

--- MbyronModsCommon/MbyronModsCommon/MessageBox/SimpleMessageBox.cs
+++ MbyronModsCommon/MbyronModsCommon/MessageBox/SimpleMessageBox.cs
@@ -1,26 +1,26 @@
-﻿using ColossalFramework.UI;
+﻿namespace MbyronModsCommon;
+using ColossalFramework.UI;
 
-namespace MbyronModsCommon {
-    public class XMLWariningMessageBox : SimpleMessageBox {
-        public UILabel WarningText { get; set; }
-        public override void Initialize<Mod>() {
-            base.Initialize<Mod>();
-            WarningText = MainPanel.AddUIComponent<UILabel>();
-            WarningText.autoSize = false;
-            WarningText.autoHeight = true;
-            WarningText.width = 560f;
-            WarningText.textScale = 1.3f;
-            WarningText.textAlignment = UIHorizontalAlignment.Center;
-            WarningText.wordWrap = true;
-            WarningText.text = CommonLocalize.XMLWariningMessageBox_Warning;
-        }
-    }
-    public class SimpleMessageBox : MessageBoxBase {
-        public SimpleMessageBox() {
-            AddButtons(1, 1, CommonLocalize.MessageBox_OK, Close);
-        }
-        public virtual void Initialize<Mod>() where Mod : IMod {
-            TitleText = ModMainInfo<Mod>.ModName;
-        }
+public class XMLWariningMessageBox : SimpleMessageBox {
+    public UILabel WarningText { get; set; }
+    public override void Initialize<Mod>() {
+        base.Initialize<Mod>();
+        WarningText = MainPanel.AddUIComponent<UILabel>();
+        WarningText.autoSize = false;
+        WarningText.autoHeight = true;
+        WarningText.width = 560f;
+        WarningText.textScale = 1.3f;
+        WarningText.textAlignment = UIHorizontalAlignment.Center;
+        WarningText.wordWrap = true;
+        WarningText.text = CommonLocalize.XMLWariningMessageBox_Warning;
     }
 }
+public class SimpleMessageBox : MessageBoxBase {
+    public SimpleMessageBox() {
+        AddButtons(1, 1, CommonLocalize.MessageBox_OK, Close);
+    }
+    public virtual void Initialize<Mod>() where Mod : IMod {
+        TitleText = ModMainInfo<Mod>.ModName;
+    }
+}
+
