a9f6fcb6ca99db1e71ba4d5570610d132d6f0211
Mbyron26 <Mbyron26@outlook.com>
2023-05-07 11:23:49 +0800

    Code cleanup.

--- MbyronModsCommon/MbyronModsCommon/ModUtils/AssemblyUtils.cs
+++ MbyronModsCommon/MbyronModsCommon/ModUtils/AssemblyUtils.cs
@@ -1,82 +1,42 @@
-﻿using ColossalFramework.Plugins;
+﻿namespace MbyronModsCommon;
+using ColossalFramework.Plugins;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 
-namespace MbyronModsCommon {
-    public class AssemblyUtils {
-        private static string currentAssemblyName;
-        private static Version currentAssemblyVersion;
-        private static string currentAssemblyPath;
-        private static List<string> folderNamesUnderCurrentAssembly;
-        private static List<string> folderNamesUnderLocalePath;
-        public static List<string> FoldersNameUnderLocalePath {
-            get {
-                if (folderNamesUnderLocalePath is null) {
-                    var path = Path.Combine(CurrentAssemblyPath, "Locale");
-                    var IDs = GetFoldersNameByPath(path).ToList();
-                    folderNamesUnderLocalePath = IDs;
-                    return IDs;
-                } else return folderNamesUnderLocalePath;
-            }
-        }
+public class AssemblyUtils {
+    private static string currentAssemblyName;
+    private static Version currentAssemblyVersion;
+    private static string currentAssemblyPath;
+    private static List<string> folderNamesUnderCurrentAssembly;
+    private static List<string> foldersNameUnderLocalePath;
 
-        public static string CurrentAssemblyName {
-            get {
-                if (currentAssemblyName is not null) return currentAssemblyName;
-                currentAssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-                return currentAssemblyName;
-            }
-        }
-        public static Version CurrentAssemblyVersion {
-            get {
-                if (currentAssemblyVersion is null) {
-                    currentAssemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
-                    return currentAssemblyVersion;
-                }
-                return currentAssemblyVersion;
-            }
-        }
+    public static string CurrentAssemblyName => currentAssemblyName ??= Assembly.GetExecutingAssembly().GetName().Name;
+    public static Version CurrentAssemblyVersion => currentAssemblyVersion ??= Assembly.GetExecutingAssembly().GetName().Version;
+    public static string CurrentAssemblyPath => currentAssemblyPath ??= GetCuttentAssemblyPath();
+    public static List<string> FoldersNameUnderLocalePath => foldersNameUnderLocalePath ??= GetFoldersNameByPath(Path.Combine(CurrentAssemblyPath, "Locale")).ToList();
+    public static List<string> FoldersNameUnderCurrentAssembly => folderNamesUnderCurrentAssembly ??= GetFoldersNameByPath(CurrentAssemblyPath).ToList();
 
-
-        public static string CurrentAssemblyPath {
-            get {
-                if (currentAssemblyPath is not null) return currentAssemblyPath;
-                currentAssemblyPath = GetCuttentAssemblyPath();
-                return currentAssemblyPath;
-            }
+    private static string GetCuttentAssemblyPath() {
+        foreach (var item in PluginManager.instance.GetPluginsInfo()) {
+            var assembliesNames = item.assembliesString;
+            if (assembliesNames.Contains(CurrentAssemblyName))
+                return item.modPath;
         }
-        public static List<string> FoldersNameUnderCurrentAssembly {
-            get {
-                if (folderNamesUnderCurrentAssembly is not null) return folderNamesUnderCurrentAssembly;
-                folderNamesUnderCurrentAssembly = GetFoldersNameByPath(CurrentAssemblyPath).ToList();
-                return folderNamesUnderCurrentAssembly;
+        return null;
+    }
+    public static string[] GetFoldersNameByPath(string path) {
+        if (Directory.Exists(path)) {
+            string[] directory = Directory.GetDirectories(path);
+            string[] names = new string[directory.Length];
+            for (int i = 0; i < directory.Length; i++) {
+                names[i] = Path.GetFileName(directory[i]);
             }
-        }
-
-        private static string GetCuttentAssemblyPath() {
-            foreach (var item in PluginManager.instance.GetPluginsInfo()) {
-                var assembliesNames = item.assembliesString;
-                if (assembliesNames.Contains(CurrentAssemblyName))
-                    return item.modPath;
-            }
+            return names;
+        } else {
             return null;
         }
-        public static string[] GetFoldersNameByPath(string path) {
-            if (Directory.Exists(path)) {
-                string[] directory = Directory.GetDirectories(path);
-                string[] names = new string[directory.Length];
-                for (int i = 0; i < directory.Length; i++) {
-                    names[i] = Path.GetFileName(directory[i]);
-                }
-                return names;
-            } else {
-                return null;
-            }
-        }
-
-
     }
 }
