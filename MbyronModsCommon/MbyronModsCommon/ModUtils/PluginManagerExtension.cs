52b4831cfe8728cb13f17f34ab5df49964b55b6b
Mbyron26 <Mbyron26@outlook.com>
2023-05-07 11:13:38 +0800

    Code cleanup

--- MbyronModsCommon/MbyronModsCommon/ModUtils/PluginManagerExtension.cs
+++ MbyronModsCommon/MbyronModsCommon/ModUtils/PluginManagerExtension.cs
@@ -1,12 +1,10 @@
-﻿using ColossalFramework.Plugins;
+﻿namespace MbyronModsCommon;
+using ColossalFramework.Plugins;
 using ColossalFramework;
 using System.Collections.Generic;
 using System.Linq;
 using ICities;
 
-namespace MbyronModsCommon {
-    public static class PluginManagerExtension {
-        public static IEnumerable<PluginManager.PluginInfo> GetPluginsInfoSortByName(this PluginManager pluginManager) => Singleton<PluginManager>.instance.GetPluginsInfo().Where(p => p?.userModInstance as IUserMod != null).OrderBy(p => ((IUserMod)p.userModInstance).Name);
-
-    }
+public static class PluginManagerExtension {
+    public static IEnumerable<PluginManager.PluginInfo> GetPluginsInfoSortByName(this PluginManager pluginManager) => Singleton<PluginManager>.instance.GetPluginsInfo().Where(p => p?.userModInstance as IUserMod != null).OrderBy(p => ((IUserMod)p.userModInstance).Name);
 }
